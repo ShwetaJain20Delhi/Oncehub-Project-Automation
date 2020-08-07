58b9f3545c9512618f48eee784be26370d6d85c6
ShwetaJain <shweta.jain@oncehub.com>
2020-08-08 01:03:59 +0530

    New files

---
+++ AutomationScript/Locators/OH_Locators/SignupAndSO_Onboarding_Locators.py
@@ -0,0 +1,125 @@
+
+class inbox_email():
+    def __init__(self, driver):
+        self.driver = driver
+    def launch_inbox_email(self):
+        self.driver.get("https://inbox.staticso2.com/")
+    def copy_inbox_email(self):
+        self.driver.find_element_by_xpath("//div[@id='div-domain']").click()
+
+
+class Signup():
+    def __init__(self, driver):
+        self.driver = driver
+        self.name = "name"
+        self.email = "email"
+        self.password = "password"
+        self.Repassword = "retypePassword"
+    def Navigate_to_URL(self):
+        self.driver.get("https://account3.onceplatform.com/signup")
+        self.driver.implicitly_wait(35)
+    def Enter_1stName(self, fname):
+        self.driver.find_element_by_name(self.name).send_keys(fname)
+    def Enter_email(self, email):
+        self.driver.find_element_by_name(self.email).send_keys(email)
+    def Enter_password(self, password):
+        self.driver.find_element_by_name(self.password).send_keys(password)
+    def ReEnter_password(self, repassword):
+        self.driver.find_element_by_name(self.Repassword).send_keys(repassword)
+    def click_signup_button(self):
+        self.driver.find_element_by_id("signUp").click()
+
+
+class Product_selection():
+    def __init__(self, driver):
+        self.driver = driver
+    def select_so_product(self):
+        self.driver.find_element_by_id("so-get-started").click()
+
+
+class Timezone_selection():
+    def __init__(self, driver):
+        self.driver = driver
+    def select_sunday(self):
+        self.driver.find_element_by_id("sunday").click()
+    def select_saturday(self):
+        self.driver.find_element_by_id("saturday").click()
+    def click_on_change_timezone(self):
+        self.driver.find_element_by_id("changetz").click()
+    def click_on_dropdown(self):
+        self.driver.find_element_by_id("selectedCountry").click()
+    def search_India(self, country):
+        self.driver.find_element_by_xpath("//input[@class='oui-select-search-input oui-input-element oui-input oui-primary cdk-text-field-autofill-monitored']").send_keys(country)
+    def select_timezone(self):
+        self.driver.find_element_by_xpath("//span[contains(text(),'India')]").click()
+    def click_save(self):
+        self.driver.find_element_by_id("saveTimeZone").click()
+    def click_continue(self):
+        flag = self.driver.find_element_by_id("continue")
+        self.driver.execute_script("arguments[0].scrollIntoView();", flag)
+        self.driver.find_element_by_id("continue").click()
+
+class Calendar_connection():
+    def __init__(self, driver):
+        self.driver = driver
+        self.email = "email"
+        self.password = "password"
+        self.ews_url = "ewsUrl"
+    def click_calendar_expand_button(self):
+        self.driver.find_element_by_xpath("// span[contains(text(), ' Using another calendar type?')]").click()
+    def click_exchange_connect_button(self):
+        self.driver.find_element_by_id("ExchangeCalendarConnectBtn").click()
+    def enter_username(self, email):
+        self.driver.find_element_by_id(self.email).send_keys(email)
+    def enter_password(self, password):
+        self.driver.find_element_by_id(self.password).send_keys(password)
+    def expand_advanced_setting(self):
+        self.driver.find_element_by_xpath("//span[contains(text(),' Advanced settings')]").click()
+    def enter_ews_url(self, url):
+        self.driver.find_element_by_id(self.ews_url).send_keys(url)
+    def click_connect_button(self):
+        self.driver.find_element_by_xpath("//button[@title='Connect']").click()
+    def click_continue(self):
+        self.driver.find_element_by_xpath("//span[contains(text(),' Continue ')]").click()
+
+
+class video_conferencing():
+    def __init__(self, driver):
+        self.driver = driver
+    def click_skip_for_video_conferencing(self):
+        self.driver.find_element_by_xpath("//a[@id='setUpLater']").click()
+
+
+class test_booking():
+    def __init__(self, driver):
+        self.driver = driver
+    def click_skip_for_test_booking(self):
+        self.driver.find_element_by_xpath("// span[contains(text(), 'Skip')]").click()
+
+
+class skip_tour():
+    def __init__(self, driver):
+        self.driver = driver
+    def click_skip_for_test_booking(self):
+        self.driver.find_element_by_xpath("//a[contains(text(),'Skip tour')]").click()
+
+
+class chatonce():
+    def __init__(self, driver):
+        self.driver = driver
+    def click_on_setup(self):
+        self.driver.find_element_by_xpath("// span[@class ='product-link']").click()
+    def select_ChatOnce(self):
+        self.driver.find_element_by_xpath("//a[contains(text(),'ChatOnce setup')]").click()
+    def click_Get_started_button_on_popup(self):
+        self.driver.find_element_by_xpath("// span[contains(text(), 'Get started')]").click()
+
+
+class logout():
+    def __init__(self, driver):
+        self.driver = driver
+    def click_profile_icon(self):
+        self.driver.implicitly_wait(20)
+        self.driver.find_element_by_xpath("//*[@id='rAccountIcon']").click()
+    def click_signout(self):
+        self.driver.find_element_by_xpath("//span[contains(text(),'Sign out')]").click()
---
+++ AutomationScript/Sign-up and Sign-in/Signup_and_SO_onboarding.py
@@ -0,0 +1,127 @@
+from AutomationScript.Locators.OH_Locators.SignupAndSO_Onboarding_Locators import inbox_email, Product_selection, \
+    Timezone_selection, Calendar_connection, video_conferencing, test_booking, Signup, skip_tour, logout, chatonce
+from AutomationScript.Webdrivers.Chrome_driver import get_chrome_driver
+import time
+from selenium.webdriver.common.keys import Keys
+
+
+class Signup_SO_Onboarding():
+    #################################  launch Chrome server  #################################
+
+    driver = None
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def Inbox_email(self):
+        inbox = inbox_email(self.driver)
+        self.driver.maximize_window()
+        inbox.launch_inbox_email()
+        time.sleep(3)
+        inbox.copy_inbox_email()
+        time.sleep(3)
+
+    def Signup_onboarding(self):
+        onboard = Signup(self.driver)
+        onboard.Navigate_to_URL()
+        time.sleep(3)
+        onboard.Enter_1stName("Test User")
+        time.sleep(2)
+        onboard.Enter_email(Keys.CONTROL + "v")
+        time.sleep(2)
+        onboard.Enter_password("testing@123")
+        time.sleep(2)
+        onboard.ReEnter_password("testing@123")
+        time.sleep(2)
+        onboard.click_signup_button()
+        time.sleep(8)
+
+    def select_product(self):
+        product = Product_selection(self.driver)
+        product.select_so_product()
+        time.sleep(10)
+
+    def select_timezone(self):
+        timezone = Timezone_selection(self.driver)
+        timezone.select_sunday()
+        time.sleep(2)
+        timezone.select_saturday()
+        time.sleep(2)
+        timezone.click_on_change_timezone()
+        time.sleep(3)
+        timezone.click_on_dropdown()
+        time.sleep(2)
+        timezone.search_India("India")
+        time.sleep(3)
+        timezone.select_timezone()
+        time.sleep(3)
+        timezone.click_save()
+        time.sleep(5)
+        timezone.click_continue()
+        time.sleep(4)
+
+    def connect_Exchange_calendar(self):
+        connect = Calendar_connection(self.driver)
+        connect.click_calendar_expand_button()
+        time.sleep(3)
+        connect.click_exchange_connect_button()
+        time.sleep(3)
+        connect.enter_username("gilad@staticso.com")
+        time.sleep(2)
+        connect.enter_password("schEdu1e")
+        time.sleep(2)
+        connect.expand_advanced_setting()
+        time.sleep(2)
+        connect.enter_ews_url("https://mex09.emailsrvr.com/owa")
+        time.sleep(2)
+        connect.click_connect_button()
+        time.sleep(10)
+        connect.click_continue()
+        time.sleep(3)
+
+    def Conferencing_option(self):
+        video = video_conferencing(self.driver)
+        video.click_skip_for_video_conferencing()
+        time.sleep(5)
+
+    def Booking_test(self):
+        test = test_booking(self.driver)
+        test.click_skip_for_test_booking()
+        time.sleep(7)
+
+    def SO_tour_skip(self):
+        tour = skip_tour(self.driver)
+        tour.click_skip_for_test_booking()
+        time.sleep(5)
+
+    def ChatOnce_started(self):
+        co_start = chatonce(self.driver)
+        co_start.click_on_setup()
+        time.sleep(3)
+        co_start.select_ChatOnce()
+        time.sleep(10)
+        co_start.click_Get_started_button_on_popup()
+        time.sleep(10)
+
+    def Application_logout(self):
+        log_out = logout(self.driver)
+        log_out.click_profile_icon()
+        time.sleep(3)
+        log_out.click_signout()
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    driver = get_chrome_driver().launch_chrome()
+    Onboard_SO = Signup_SO_Onboarding(driver)
+    Onboard_SO.Inbox_email()
+    Onboard_SO.Signup_onboarding()
+    Onboard_SO.select_product()
+    Onboard_SO.select_timezone()
+    Onboard_SO.connect_Exchange_calendar()
+    Onboard_SO.Conferencing_option()
+    Onboard_SO.Booking_test()
+    Onboard_SO.SO_tour_skip()
+    Onboard_SO.ChatOnce_started()
+    Onboard_SO.Application_logout()
+    driver.close()
